d721dd5d29c01e125a8be90137ff4ed1f631d8c9
Dayana <dayana.azzam@galactech.io>
2022-07-11 14:27:30 +0300

    Edit Listing

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,12 @@ Route::get('/listings/create', [ListingController::class,'create']);
 //Store Listing Data
 Route::post('/listings', [ListingController::class,'store']);
 
+//Show Edit Form
+Route::get('/listings/{listing}/edit',
+[ListingController::class,'edit']);
 
-
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class,'update']);
 
 //SINGLE LISTING 
 Route::get('/listings/{listing}', [ListingController::class,'show']);
