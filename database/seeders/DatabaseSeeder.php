b5b76c933c15c8595ddf118d22633ae05a46b90d
Dayana <dayana.azzam@galactech.io>
2022-07-11 19:34:19 +0300

    Add Ownership T Litings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Listing;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,8 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
-        Listing::factory(6)->create();
+        // \App\Models\User::factory(5)->create();
+        $user = User::factory()->create([
+            'name' =>'John Doe',
+            'email'=>'john@gmail.com'
+        ]);
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
         // Listing::create([
         //     'title' => 'Laravel Senior Developer', 
         //         'tags' => 'laravel, javascript',
