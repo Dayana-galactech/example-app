49cb5cc821bc17bb781cd9ce6f17ed071e47cd5b
Dayana <dayana.azzam@galactech.io>
2022-07-11 15:24:27 +0300

    User Registration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -36,6 +37,12 @@ Route::delete('/listings/{listing}', [ListingController::class,'destroy']);
 
 //SINGLE LISTING 
 Route::get('/listings/{listing}', [ListingController::class,'show']);
+
+//Show Register/Create Form
+Route::get('/register',[UserController::class,'create']);
+
+//Create new User
+Route::post('/users',[UserController::class,'store']);
 
 // Route::get('/', function () {
 //     return view('welcome');
